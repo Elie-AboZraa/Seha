eebec5d2db8940b1d4cf5530cee5dc811e574768
Elie-AboZraa <elieaboozraa@gmail.com>
2025-06-20 17:57:51 +0300

    add the control to the buttons so you can move between the pages

--- src/DataSaverController.java
+++ src/DataSaverController.java
@@ -1,8 +1,16 @@
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 import java.sql.*;
 import java.util.function.Consumer;
 
@@ -22,6 +30,32 @@ public class DataSaverController {
         
         // Load initial data
         loadDataFromDatabase();
+    }
+     @FXML
+    void BackCreateLeave(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/Scene/CreateLeavePage.fxml"));
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root, 1400, 900);
+        scene.getStylesheets().add(getClass().getResource("/Scene/SceneStyle/CreateLeavePage.css").toExternalForm());
+        stage.setScene(scene);
+        stage.show();
+    }
+      @FXML
+    void theDataPage(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/Scene/dataSaver.fxml"));
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root, 1400, 900);  // Add dimensions
+        stage.setScene(scene);
+        stage.show();
+    }
+    @FXML
+    void BackToHomePAge(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/Scene/HomePage.fxml"));
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root, 1400, 900);
+        scene.getStylesheets().add(getClass().getResource("/Scene/SceneStyle/HomePage.css").toExternalForm());
+        stage.setScene(scene);
+        stage.show();
     }
 
     private void setupButtonColumns() {
@@ -194,4 +228,5 @@ public class DataSaverController {
         public String getNameArabic() { return nameArabic; }
         public String getIdNumber() { return idNumber; }
     }
+    
 }
